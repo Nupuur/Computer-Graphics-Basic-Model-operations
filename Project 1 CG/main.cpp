f66ba157a99045e4c49ad430b4f87680c4f9f7bf
Nupur <Nupur@Users-MacBook-Pro.local>
2017-03-02 12:26:55 -0600

    Initial Commit

---
+++ Project 1 CG/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project 1 CG
+//
+//  Created by Nupur on 3/2/17.
+//  Copyright © 2017 Nupur. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
